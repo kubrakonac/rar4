d5437ba52834844655d402ff8124ecde16599930
kubrakonac <51789393+kubrakonac@users.noreply.github.com>
2021-01-10 16:13:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,165 @@
+$(document).ready(function(){
+
+    // banner owl carousel
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots: true,
+        items: 1
+    });
+
+    // top sale owl carousel
+    $("#cok-satanlar .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+
+    // isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector : '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+    // filter items on button click
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue});
+    })
+
+
+    // new phones owl carousel
+    $("#yeni-telefonlar .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+
+    // blogs owl carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    });
+
+    var testimonialSlider = $(".testimonial__slider");
+    testimonialSlider.owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false,
+        responsive: {
+            992: {
+                items: 3
+            },
+            768: {
+                items: 2
+            },
+            320: {
+                items: 1
+            }
+        }
+    });
+
+
+
+    // product qty section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    let $deal_price = $("#deal-price");
+    let $total_cart_amt = $("#total_cart_amt");
+    // let $input = $(".qty .qty_input");
+
+    // click on qty up button
+    $qty_up.click(function(e){
+
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        let $price = $(`.product_price[data-id='${$(this).data("id")}']`);
+
+        // change product price using ajax call
+        $.ajax({url: "Template/ajax.php", type : 'post', data : { itemid : $(this).data("id")}, success: function(result){
+                let obj = JSON.parse(result);
+                let item_price = obj[0]['item_price'];
+
+                if($input.val() >= 1 && $input.val() <= 9){
+                    $input.val(function(i, oldval){
+                        return ++oldval;
+                    });
+
+                    // increase price of the product
+                    $price.text(parseInt(item_price * $input.val()).toFixed(2));
+
+                    // set subtotal price
+                    let subtotal = parseInt($deal_price.text()) + parseInt(item_price);
+                    $deal_price.text(subtotal.toFixed(2));
+                    let totalcartamt = subtotal + 5;
+                    $total_cart_amt.text(totalcartamt.toFixed(2));
+                }
+
+            }}); // closing ajax request
+    }); // closing qty up button
+
+    // click on qty down button
+    $qty_down.click(function(e){
+
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        let $price = $(`.product_price[data-id='${$(this).data("id")}']`);
+
+        // change product price using ajax call
+        $.ajax({url: "Template/ajax.php", type : 'post', data : { itemid : $(this).data("id")}, success: function(result){
+                let obj = JSON.parse(result);
+                let item_price = obj[0]['item_price'];
+
+                if($input.val() > 1 && $input.val() <= 10){
+                    $input.val(function(i, oldval){
+                        return --oldval;
+                    });
+
+
+                    // increase price of the product
+                    $price.text(parseInt(item_price * $input.val()).toFixed(2));
+
+                    // set subtotal price
+                    let subtotal = parseInt($deal_price.text()) - parseInt(item_price);
+                    $deal_price.text(subtotal.toFixed(2));
+                    let totalcartamt = subtotal + 5;
+                    $total_cart_amt.text(totalcartamt.toFixed(2));
+                }
+
+            }}); // closing ajax request
+    }); // closing qty down button
+
+
+
+});
